c3bb16c83f21cd2760dd7ac13a3a93255755d7e5
msadows1 <01178588@pw.edu.pl>
2025-11-01 20:06:19 +0100

    small changes

--- narrion/tabs/generator/names.py
+++ narrion/tabs/generator/names.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import (
     QGroupBox,
     QLabel,
     QPushButton,
+    QSizePolicy,
     QVBoxLayout,
     QWidget,
 )
@@ -28,33 +29,35 @@ class NameWidget(QWidget):
 
         for i in range(16):
             label = QLabel("")
+            label.setWordWrap(True)
             self.labels.append(label)
             self.grid.addWidget(label, i // 4, (i % 3) * 2, 1, 2)
 
-        self.grid.addWidget(QLabel("Type:"), 4, 1)
-        self.grid.addWidget(QLabel("Sex:"), 4, 4)
+        self.grid.addWidget(QLabel("Type:"), 4, 0)
+        self.grid.addWidget(QLabel("Sex:"), 5, 0)
 
         name_type_select = QComboBox()
         name_type_select.addItems(["Human", "Dwarf", "Elf", "Hobbit", "French", "Anglo"])
         name_type_select.currentTextChanged.connect(self.select_generator_type)
-        self.grid.addWidget(name_type_select, 5, 0, 1, 3)
+        self.grid.addWidget(name_type_select, 4, 1, 1, 2)
 
         sex_type_select = QComboBox()
         sex_type_select.addItems(["male", "female", "sometimes"])
         sex_type_select.currentTextChanged.connect(self.select_sex)
-        self.grid.addWidget(sex_type_select, 5, 3, 1, 3)
+        self.grid.addWidget(sex_type_select, 5, 1, 1, 2)
 
         self.button = QPushButton("Generate")
         self.button.setObjectName("rollButton")
         self.button.clicked.connect(self.generate_names)
-        self.grid.addWidget(self.button, 6, 0, 1, 6)
+        self.button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.grid.addWidget(self.button, 4, 3, 2, 3)
 
         self.v.addWidget(self.box)
         self.function = names.human
         self.sex = "male"
 
     def select_generator_type(self, type: str):
-        self.function = self.function_map(type)
+        self.function = self.function_map[type]
 
     def select_sex(self, sex: str):
         self.sex = sex if sex != "sometimes" else "any"
