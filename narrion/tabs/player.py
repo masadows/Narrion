45b6c8f16f4c36dbd242c044dd6c315cd86d3e9e
msadows1 <01178588@pw.edu.pl>
2025-11-06 09:58:01 +0100

    change method of displaying files

--- narrion/tabs/player.py
+++ narrion/tabs/player.py
@@ -1,7 +1,6 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QColor, QPainter, QPixmap
 from PySide6.QtPdf import QPdfDocument
-from PySide6.QtPdfWidgets import QPdfView
 from PySide6.QtWidgets import (
     QFileDialog,
     QHBoxLayout,
@@ -9,6 +8,7 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QPushButton,
     QScrollArea,
+    QSizePolicy,
     QTextEdit,
     QVBoxLayout,
     QWidget,
@@ -25,120 +25,119 @@ class CharacterWidget(QWidget):
 
         scroll = QScrollArea()
         scroll.setWidgetResizable(True)
-
         container = QWidget()
         container_layout = QVBoxLayout(container)
 
         header_layout = QHBoxLayout()
         header_layout.addWidget(SectionHeader(name))
-
         change_btn = QPushButton()
-        icon = qta.icon("mdi.file-account")
-        change_btn.setIcon(icon)
+        change_btn.setIcon(qta.icon("mdi.file-account"))
         change_btn.setToolTip("Zmień kartę postaci")
         change_btn.setIconSize(QSize(24, 24))
         change_btn.setMaximumWidth(40)
-        change_btn.clicked.connect(self.changePlayerFlie)
+        change_btn.clicked.connect(self.changePlayerFile)
         header_layout.addWidget(change_btn)
-
         container_layout.addLayout(header_layout)
-        container_layout.addWidget(QLineEdit())
-        text_edit = QTextEdit()
-        text_edit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        text_edit.textChanged.connect(
-            lambda te=text_edit: te.setFixedHeight(
-                max(
-                    int(
-                        te.document().size().height()
-                        + te.contentsMargins().top()
-                        + te.contentsMargins().bottom()
-                        + 4
-                    ),
-                    70,
-                )
-            )
-        )
-        container_layout.addWidget(text_edit)
+        container_layout.addWidget(QLineEdit())
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.text_edit.textChanged.connect(self.adjustTextEditHeight)
+        container_layout.addWidget(self.text_edit)
 
         self.image_label = QLabel()
         self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.image_label.hide()
         container_layout.addWidget(self.image_label)
-
-        self.pdf_doc = QPdfDocument(self)
-
-        self.pdf_view = QPdfView(self)
-        self.pdf_view.setDocument(self.pdf_doc)
-        self.pdf_view.setPageMode(QPdfView.PageMode.MultiPage)
-
-        self.pdf_view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.pdf_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-        self.pdf_view.setZoomMode(QPdfView.ZoomMode.Custom)
-
-        self.setPdfSize()
-
-        container_layout.addWidget(self.pdf_view)
 
         container_layout.addStretch()
         scroll.setWidget(container)
         self.layout.addWidget(scroll)
 
-    def changePlayerFlie(self):
+        self.pdf_doc = None
+        self.original_pixmap = None
+
+    def adjustTextEditHeight(self):
+        te = self.text_edit
+        height = max(
+            int(
+                te.document().size().height()
+                + te.contentsMargins().top()
+                + te.contentsMargins().bottom()
+                + 4
+            ),
+            70,
+        )
+        te.setFixedHeight(height)
+
+    def changePlayerFile(self):
         path_file, _ = QFileDialog.getOpenFileName(
             self,
             "Wybierz kartę postaci",
             "",
             "PDF i obrazy (*.pdf *.png *.jpg *.jpeg *.bmp *.gif);;Wszystkie pliki (*)",
         )
+        if not path_file:
+            return
 
-        if path_file.lower().split(".")[-1] == "pdf":
-            self.pdf_doc.load(path_file)
-            self.image_label.setPixmap(QPixmap())
-            self.image_label.hide()
+        if path_file.lower().endswith(".pdf"):
+            self.loadPdfAsPixmap(path_file)
         else:
-            self.pdf_doc = QPdfDocument()
-            self.original_pixmap = QPixmap(path_file)
-            self.updateImageSize()
+            self.loadImage(path_file)
 
-    def setPdfSize(self):
-        if self.pdf_doc.status() == QPdfDocument.Status.Ready:
-            page_width_pt = self.pdf_doc.pagePointSize(0).width() + 20
-            scale = self.pdf_view.width() * 72 / (96 * page_width_pt)
+    def loadPdfAsPixmap(self, path):
+        self.pdf_doc = QPdfDocument(self)
+        self.pdf_doc.load(path)
+        if self.pdf_doc.status() != QPdfDocument.Status.Ready:
+            return
 
-            self.pdf_view.setZoomFactor(scale)
+        images = []
+        total_height = 0
+        max_width = 0
 
-            total_height = (
-                sum(
-                    self.pdf_doc.pagePointSize(i).height() * scale * 96 / 72
-                    for i in range(self.pdf_doc.pageCount())
-                )
-                + 40
-            )
-            self.pdf_view.setFixedHeight(int(total_height))
-        else:
-            self.pdf_view.setFixedHeight(0)
+        for i in range(self.pdf_doc.pageCount()):
+            page_size = self.pdf_doc.pagePointSize(i)
+            width_px = int(page_size.width() * 4 / 3)
+            height_px = int(page_size.height() * 4 / 3)
+            image = self.pdf_doc.render(i, QSize(width_px, height_px))
+            if image.isNull():
+                continue
+            images.append(image)
+            total_height += image.height()
+            max_width = max(max_width, image.width())
+
+        if not images:
+            return
+
+        combined = QPixmap(max_width, total_height)
+        combined.fill(QColor("white"))
+        painter = QPainter(combined)
+        y_offset = 0
+        for img in images:
+            painter.drawImage(0, y_offset, img)
+            y_offset += img.height()
+        painter.end()
+
+        self.original_pixmap = combined
+        self.updateImageSize()
+        self.image_label.show()
+
+    def loadImage(self, path):
+        self.original_pixmap = QPixmap(path)
+        self.updateImageSize()
+        self.image_label.show()
+        self.pdf_doc = None
 
     def updateImageSize(self):
-        if self.original_pixmap:
-            self.image_label.show()
-            scaled = self.original_pixmap.scaled(
-                self.image_label.width() - 30,
-                self.original_pixmap.height(),
-                Qt.KeepAspectRatio,
-                Qt.SmoothTransformation,
-            )
-            self.image_label.setPixmap(scaled)
-        else:
+        if self.original_pixmap is None:
             self.image_label.hide()
+            return
+        scaled = self.original_pixmap.scaledToWidth(self.width() - 70, Qt.SmoothTransformation)
+        self.image_label.setPixmap(scaled)
+        self.image_label.show()
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
-        if hasattr(self, "pdf_view") and self.pdf_view:
-            self.setPdfSize()
-
-        if self.image_label.pixmap():
-            pixmap = self.image_label.pixmap()
-            if not pixmap.isNull():
-                self.updateImageSize()
+        self.updateImageSize()
