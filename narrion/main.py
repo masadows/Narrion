198e8b007729fb3467d2adf67b7ae65568ea719e
Michal Pienkos <michal@desktop-t3drurn.home>
2026-01-09 11:38:41 +0100

    Fixed polish confirmations

--- narrion/main.py
+++ narrion/main.py
@@ -2,11 +2,15 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTranslator, QLibraryInfo
 
 from main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    translator = QTranslator()
+    translator.load("qtbase_pl", QLibraryInfo.path(QLibraryInfo.TranslationsPath))
+    app.installTranslator(translator)
     win = MainWindow()
     win.show()
     win.showNormal()
